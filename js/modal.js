8d342a6e2d7c7c3c4dfe4d1095d533a71c8b7eff
OverOneYeti <90979377+LizzieMonk@users.noreply.github.com>
2023-08-31 17:27:07 +0300

    feat: create modal

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById('modal');
+var modalInput = document.getElementById('modal-input');
+
+window.addEventListener('load', ()=>{
+    modal.style.display = "block";
+})
+modalInput.addEventListener('input', ()=>{
+    if(modalInput.value.toLowerCase() ==='helen'
+    || modalInput.value.toLowerCase() === 'елена'){
+        modal.style.display = "none";
+    }
+})
