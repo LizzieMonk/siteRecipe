c8efb3b770f6c5d380426e2110930da1ee89026e
OverOneYeti <90979377+LizzieMonk@users.noreply.github.com>
2023-09-01 08:15:52 +0300

    fix(modal.js): hide modal

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,9 @@
 const modal = document.getElementById('modal');
 var modalInput = document.getElementById('modal-input');
 
-window.addEventListener('load', ()=>{
-    modal.style.display = "block";
-})
+// window.addEventListener('load', ()=>{
+//     modal.style.display = "block";
+// })
 modalInput.addEventListener('input', ()=>{
     if(modalInput.value.toLowerCase() ==='helen'
     || modalInput.value.toLowerCase() === 'елена'){
